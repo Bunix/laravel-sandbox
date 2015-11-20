be71ba4301dfb60e824bbdebbd44ef0beb034693
Eric Mitkowski <eric.mitkowski@rightstart.com>
2015-11-20 14:51:29 -0700

    refactor models dir and add users, admins, customers

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models\User;
 
-use App\Repositories\EloquentAbstract;
+use App\Models\ModelAbstract;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends EloquentAbstract implements AuthenticatableContract,
+class User extends ModelAbstract implements AuthenticatableContract,
     AuthorizableContract,
     CanResetPasswordContract,
     UserInterface

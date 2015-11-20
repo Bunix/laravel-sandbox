1d404a18d4ac559cbe93c349a372b8993bdb6798
Eric Mitkowski <eric.mitkowski@rightstart.com>
2015-11-20 15:35:18 -0700

    add user factory and seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -5,31 +5,11 @@ use Illuminate\Database\Seeder;
 class UserTableSeeder extends Seeder
 {
 
-    private $seed_number = 200;
+    private $seed_number = 10;
 
     public function run()
     {
-        // Set DB and clear
-        $db = DB::table('users');
-        $db->truncate();
-
-        // Get Repo
-        $users_repo = new \App\Repositories\User\UsersEloquent();
-
-        // Faker
-        $faker = Faker\Factory::create();
-
-        // Run Seed
-        for ($i = 0; $i < $this->seed_number; $i++) {
-            $users_repo->createRow([
-                'email' => $faker->email,
-                'first_name' => $faker->firstName,
-                'last_name' => $faker->lastName,
-                'zip_code' => $faker->postcode,
-                'confirmed' => $faker->numberBetween(0, 1),
-            ]);
-        }
-
+        factory(App\Models\User\User::class, $this->seed_number)->create();
     }
 
 }
